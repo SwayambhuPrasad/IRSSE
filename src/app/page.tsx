a12c027629edfe411d52ecc1d31ec21950237e0e
SwayambhuQube <swayambhu.panda@qubecinema.com>
2023-08-26 14:19:30 +0530

    Initial setup done, Navigation bar added

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import NavigationBar from "@/components/NavigationBar";
+
+
+// 1. import `NextUIProvider` component
+
+
+export default function App() {
+  // 2. Wrap NextUIProvider at the root of your app
+  return (
+     <NavigationBar/>
+  );
+}
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,64 @@
+'use client'
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import Image from "next/image";
+
+export default function NavigationBar() {
+    return(
+        <Navbar>
+        <NavbarBrand>
+          <Image src="/logo.jpeg"
+      width={60}
+      height={60}
+      alt="Picture of the author"/>
+          <p className="font-bold text-inherit">IRSSE</p>
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Home
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive>
+            <Link href="#" aria-current="page">
+              About
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              For IRSSE
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Resources
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Articles
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Gallery
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Contact Us
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem className="hidden lg:flex">
+            <Link href="#">Login</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Button as={Link} color="primary" href="#" variant="flat">
+              Sign Up
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    )
+}
