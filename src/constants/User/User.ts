efd9c06956797b94abb7fafbb01fb545923612c7
SwayambhuQube <swayambhu.panda@qubecinema.com>
2023-09-09 11:50:27 +0530

    User Added

---
+++ src/constants/User/User.ts
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    id: "1",
+    name: "Swayambhu",
+    email: "swayambhu@qwe.com",
+    password: "1111",
+  },
+  {
+    id: "2",
+    name: "Rupesh",
+    email: "rupesh@qwe.com",
+    password: "2222",
+  },
+];
